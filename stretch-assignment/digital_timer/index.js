a5b0f47f05e63bb96730b5d8937fe9cc88adc9e3
Mckay <mckaywrigley@gmail.com>
2019-03-04 11:36:24 -0700

    stretch timer works

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,16 +1,39 @@
 // Body Selector
-const body = document.querySelector('body');
+let body = document.querySelector('body');
 
-// Digits Selector
-const digits = document.querySelector('.digits');
+// Digits Selectors
+let digits = document.querySelector('.digits');
+let secondTens = document.querySelector('#secondTens');
+let secondOnes = document.querySelector('#secondOnes');
+let msHundreds = document.querySelector('#msHundreds');
+let msTens = document.querySelector('#msTens');
 
 // Create Start Timer Button
-const createButton = document.createElement('p');
-createButton.textContent = 'Start Timer';
-createButton.style.border = '1px solid black';
-createButton.style.padding = '5px';
+let button = document.createElement('p');
+button.textContent = 'Start Timer';
+button.style.border = '1px solid black';
+button.style.padding = '5px';
+button.style.marginLeft = '20px';
 digits.style.display = 'flex';
 digits.style.flexWrap = 'flex-wrap';
 digits.style.justifyContent = 'center';
-createButton.style.width = '50px';
-digits.appendChild(createButton);
+button.style.width = '100px';
+button.style.textAlign = 'center';
+digits.appendChild(button);
+
+let secondsOnesDigit = 0;
+let msHundredsDigit = 0;
+
+// Button Click Handler
+button.addEventListener('click', () => {
+    setInterval(() => {
+        msHundreds.textContent = msHundredsDigit;
+        secondOnes.textContent = secondsOnesDigit;
+        if(msHundredsDigit < 99) {
+            msHundredsDigit++;
+        } else if (msHundredsDigit === 99) {
+            msHundredsDigit = 0;
+            secondsOnesDigit++;
+        }
+    }, 10)
+});
